adc3b215bac9e1ea33088903c5838d5bd2f36728
Hack Reactor <hackreactor@Hacks-Mac-mini-2.local>
2014-03-10 15:17:38 -0700

    display retrieved messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,26 @@
-// YOUR CODE HERE:
+var getPosts = function(){
+  $.ajax({
+    // always use this url
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'GET',
+    success: function (data) {
+      displayPosts(data.results);
+    },
+    error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
+
+var displayPosts = function(data){
+  for (var i = 0; i < data.length; i++) {
+    var username = data[i]['username'];
+    var text = data[i]['text'];
+    $('.posts').append('<p>' + username + ': ' + text + '</p>');
+  };
+};
+
+getPosts();
+
 
