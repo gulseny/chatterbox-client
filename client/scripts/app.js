23cfb8577d72c4f121027166f5537733623ddf4f
Hack Reactor <hackreactor@Hacks-Mac-mini-2.local>
2014-03-10 17:31:14 -0700

    Get usernamr function added

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,40 +1,57 @@
-var getPosts = function(){
-  $.ajax({
-    // always use this url
-    url: 'https://api.parse.com/1/classes/chatterbox',
-    type: 'GET',
-    success: function (data) {
-      displayPosts(data.results);
-    },
-    error: function (data) {
-      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to send message');
+$(document).ready(function(){
+
+
+
+  var getPosts = function(){
+    $.ajax({
+      // always use this url
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'GET',
+      success: function (data) {
+        displayPosts(data.results);
+      },
+      error: function (data) {
+        // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message');
+      }
+    });
+  };
+
+  var displayPosts = function(data){
+    var messages = [];
+    for (var i = 0; i < data.length; i++) {
+      // console.log(data[i]['username'],data[i]['text']);
+      var username = (data[i]['username']) ? data[i]['username'] : "Guest";
+      var text = (validateData(data[i]['text'])) ? data[i]['text'] : "ILLEGAL XSS";
+
+      messages.push('<p>' + username + ': ' + text+ '</p>');
     }
+    $('.posts').append(messages);
+  };
+
+  var validateData = function(data){
+    if(typeof data === 'string'){
+      if(data.charAt(0) === '<'){
+        return false;
+      }else{
+        return true;
+      }
+    }
+
+  };
+
+  getPosts();
+
+  // setInterval(getPosts, 5000);
+ $('#joined').on('click',function(){
+    var newUser = $('#name').val();
+    // $('.enterName').toggle();
+    // $('.enterMessage').toggle();
+  console.log(newUser);
   });
-};
 
-var displayPosts = function(data){
-  var messages = [];
-  for (var i = 0; i < data.length; i++) {
-    console.log(data[i]['username'],data[i]['text']);
-    messages.push('<p>' + data[i]['username'] + ': ' + data[i]['text']+ '</p>');
-  }
-  $('.posts').append(messages);
-};
+  
+});
 
-var validateData = function(data){
-  if(typeof data === 'string'){
-    if(data.charAt(0) === '<'){
-      return true;
-    }else{
-      return false;
-    }
-  }
-
-};
-
-getPosts();
-
-// setInterval(getPosts, 5000);
 
 
